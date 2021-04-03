8e06f4cf5c165b5ba83d25ae7a1ecaaf8c26074f
oscarmtzo <icehman@hotmail.com>
2021-04-02 18:20:40 -0600

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,92 +1,111 @@
-// Characters
-const mrGreen = {
-    first_name: 'Jacob',
-    last_name: 'Green',
-    color: 'green',
-    description:  'He has a lot of connections',
-    age: 45,
-    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
-    occupation: 'Entrepreneur'
-}
-const profPlum = {
-    first_name:   'Victor',
-    last_name:    'Plum',
-    color:        'purple',
-    description:  'Billionaire video game designer',
-    age:          22,
-    image:        'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
-    occupation:   'Designer'
-}
-const missScarlet = {
-    first_name:   'Kasandra',
-    last_name:    'Scarlet',
-    color:        'red',
-    description:  'She is an A-list movie star with a dark past',
-    age:          31,
-    image:        'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
-    occupation:   'Actor'    
-}
-const mrsPeacock = {
-    first_name:   'Eleanor',
-    last_name:    'Peacock',
-    color:        'blue',
-    description:  'She is from a wealthy family and uses her status and money to earn popularity',
-    age:          36,
-    image:        'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
-    occupation:   'Socialité'
-}
-const mrMustard ={
-    first_name:   'Jack',
-    last_name:    'Mustard',
-    color:        'yellow',
-    description:  'He is a former football player who tries to get by on his former glory',
-    age:          62,
-    image:        'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
-    occupation:   'Retired Football player'
-}
-// Weapons
-
-const rope = {weight: 10}
-const  knife = {weight: 8}
-const candlestick  = {weight: 2}
-const dumbbell = {weight: 30}
-const poison = {weight: 2}
-const axe = {weight: 15}
-const bat = {weight: 13}
-const trophy = {weight: 25}
-const pistol = {weight: 20}
-
-// Rooms
-
-
-// Characters Collection
-const charactersArray = [mrGreen, profPlum, missScarlet, mrsPeacock, mrMustard];
-
+// ITERATION 1
+// Suspects Collection
+const charactersArray = [
+    {
+        firstName: 'Jacob',
+        lastName: 'Green',
+        occupation: 'Entrepreneur',
+        age: 45,
+        description: 'He has a lot of connections',
+        image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+        color: 'green'},
+        {
+            firstName: 'Doctor',
+            lastName: 'Orchid',
+            occupation: 'Scientist',
+            age: 26,
+            description: 'PhD in plant toxicology. Adopted daughter of Mr.Boddy',
+            image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+            color: 'white'
+        },    
+        {
+            firstName: 'Victor',
+            lastName: 'Plum',
+            occupation: 'Designer',
+            age: 22,
+            description: 'Billionaire video game designer',
+            image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+            color: 'purple'
+        },
+        {
+            firstName: 'Kasandra',
+            lastName: 'Scarlet',
+            occupation: 'Actor',
+            age: 31,
+            description: 'She is an A-list movie star with a dark past',
+            image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+            color: 'red'
+        },    
+        {
+            firstName: 'Eleanor',
+            lastName: 'Peacock',
+            occupation: 'Socialité',
+            age: 36,
+            description: 'She is from a wealthy family and uses her status and money to earn popularity',
+            image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+            color: 'blue'
+        },
+        {
+            firstName: 'Jack',
+            lastName: 'Mustard',
+            occupation: 'Retired Football player',
+            age: 62,
+            description: 'He is a former football player who tries to get by on his former glory',
+            image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+            color: 'yellow'
+        }
+];
+    
 // Rooms Collection
-const roomsArray = ['Dining Room', 'Conservatory', 'Kitchen', 'Study', 'Library', 'Billiard Room', 'Lounge', 'Ballroom', 'Hall', 'Spa', 'Living Room', 'Observatory', 'Theater', 'Guest House', 'Patio'];
+const roomsArray = [
+    'Dining Room',
+    'Conservatory',
+    'Kitchen',
+    'Study',
+    'Library',
+    'Billiard Room',
+    'Lounge',
+    'Ballroom',
+    'Hall',
+    'Spa',
+    'Living Room',
+    'Observatory',
+    'Theater',
+    'Guest House',
+    'Patio'
+];
 
 // Weapons Collection
-const weaponsArray = [rope, knife, candlestick, dumbbell, poison, axe, bat, trophy, pistol]
+const weaponsArray = [
+    {name: 'rope',  weight: 10},
+    {name: 'knife',  weight: 8},
+    {name: 'candlestick',  weight: 2},
+    {name: 'dumbbell',  weight: 30},
+    {name: 'poison',  weight: 2},
+    {name: 'axe',  weight: 15},
+    {name: 'bat',  weight: 13},
+    {name: 'trophy', weight: 25},
+    {name: 'pistol',  weight: 20}
+];
 
-const randomSelector = (arr) =>{
-    if(arr.length==0){
-        return undefined
-    }else {
-        return arr[Math.floor(Math.random() * arr.length)]
-    }
+// ITERATION 2
+function randomSelector(arr){
+    number= Math.floor(Math.random()*arr.length)
+    if(arr.length ===1){ return arr[0]}
+    return arr[number]
 }
-const pickMistery = (arr, suspect, weapons, rooms) => {
-    
-    arr =[]
-    suspect = randomSelector(charactersArray)
-    weapons = randomSelector(weaponsArray)
-    rooms = randomSelector(roomsArray)
-    arr.push(suspect, weapons, rooms )
+
+function pickMistery(){
+    arr=[
+        randomSelector(charactersArray),
+        randomSelector(weaponsArray),
+        randomSelector(roomsArray)
+    ]
+
     return arr
 }
-const revealMistery = (mysteryEnvelope) => {
-    mysteryEnvelope = {}
-    return mysteryEnvelope
+// ITERATION 3
+function revealMistery(arr){
+    console.log(arr)
+    return `${arr[0].first_name} ${arr[0].last_name} killed Mr.Boddy using the ${arr[1].name} in the ${arr[2].name}!!!!`
 }
-
-   
